55090307ca0ac6351509572c5d539652113e4e0b
Faizack <faizack619@gmail.com>
2025-09-03 04:52:05 +0530

    updated with pytorch

--- NLP/NLP.py
+++ NLP/NLP.py
@@ -4,10 +4,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from textblob import TextBlob
 from transformers import pipeline
 import numpy as np
+
 # print("numpy version is " + np.__version__)
 
 # Download necessary NLTK datasets
-nltk.download('punkt')
+nltk.download("punkt")
 
 # Sample text
 document = "Natural language processing (NLP) is a field of AI that helps machines understand human language. NLP is used in various applications like sentiment analysis, machine translation, and text summarization."
@@ -31,7 +32,9 @@ print("Sentiment Polarity:", blob.sentiment.polarity)
 # Named Entity Recognition (NER)
 
 # Load the NER pipeline
-ner_pipeline = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english")
+ner_pipeline = pipeline(
+    "ner", model="dbmdz/bert-large-cased-finetuned-conll03-english", framework="pt"
+)
 # Sample text
 sample_text = "Sundar Pichai, the CEO of Google, announced a partnership with Microsoft in California."
 # Perform NER
@@ -39,18 +42,19 @@ ner_results = ner_pipeline(sample_text)
 
 # Print named entities
 for entity in ner_results:
-    print(f"Entity: {entity['word']}, Label: {entity['entity']}, Score: {entity['score']:.4f}")
+    print(
+        f"Entity: {entity['word']}, Label: {entity['entity']}, Score: {entity['score']:.4f}"
+    )
 
 
 # Text Summarization
 summarizer = pipeline("summarization")
 summarized_text = summarizer(document, max_length=50, min_length=25, do_sample=False)
-print("Summarized Text:", summarized_text[0]['summary_text'])
+print("Summarized Text:", summarized_text[0]["summary_text"])
 
 # Machine Translation (English to French)
 # translator = pipeline("translation_en_to_fr")
 # Load the English-to-Chinese translation model
 translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-zh")
 translated_text = translator(document)
-print("Translated Text ():", translated_text[0]['translation_text'])
-
+print("Translated Text ():", translated_text[0]["translation_text"])
